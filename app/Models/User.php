193064c677951efca4342b3f7a5a96814d9b2802
Olaoye Sunday <olaoyesunday@yahoo.com>
2024-05-27 10:16:07 +0100

    trying various ways of using crud

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+//use Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -44,4 +48,26 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    // To change name to uppercase use Accessor(An accessor transforms an Eloquent attribute value when it is accessed. )
+    // protected function name(): Attribute
+    // {
+    //     return Attribute::make(
+    //         get: fn (string $value) => strtoupper($value)
+    //     );
+    // }
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => strtoupper($value),
+        );
+    }
+    // To encrypt a password if it has not yet encrypted (This is what we called mutator)
+    /* protected function password(): Attribute
+    {
+        return Attribute::make(
+            set: fn($value) => bcrypt($value)
+        );
+    } */
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,92 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
-
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    
+    //FETCH ALL USERS
+    //$users = DB::select('select * from users');
+
+    //create new user
+    // $user = DB::insert('insert into users (name, email, password) values(?,?,?)', [
+    //     'Joseph',
+    //     'joseph@gmial.com',
+    //     'Joseph_123'
+    // ]);
+
+    //update user
+    //$update = DB::update('update users set email="josepholaoye@gmail.com" where id=2'); 
+    //you can also use binding
+        // $update = DB::update("update users set email=? where id=?", [
+        //     'joseph@gmail.com',
+        //     '2'
+        // ]); 
+    
+    // Delete user
+    //$delete = DB::delete("DELETE FROM users WHERE id=?", [2]);
+
+    //dd($users);
+
+  // LET US USE QUERY BUILDER TO PERFORM ALL WHAT WE HAVE DONE ABOVE
+
+  // FETCH ALL USERS
+   //$users = DB::table('users')->get();
+ //  $users = DB::table('users')->find(4);   
+
+   // Create a new user
+//    $user = DB::table('users')->insert([
+//     'name' => 'Favour',
+//     'email' => 'favour@gmail.com',
+//     'password' => 'Favour_123'
+// ]);
+// insert several records at once
+    // $user = DB::table('users')->insert([        
+    //     ['name' => 'Elizabeth', 'email' => 'elizabeth@gmail.com', 'password' => 'Elizabeth_123'],
+    //     ['name' => 'Treasure','email' => 'treasure@gmail.com', 'password' => 'Treasure_123'],
+    // ]);
+
+    // update a user
+    //$update = DB::table('users')->where('id', 5)->update(['email'=> 'gracefemitayo@gmail.com']);
+    
+    // Delete  user
+    //$delete = DB::table('users')->where('id', 5)->delete();
+
+    //LET US USE ELOQUENT TO PERFORM ALL WHAT WE HAVE DONE ABOVE
+    // FETCH ALL USERS
+    //$users = User::get();
+    //$users = User::find(12);
+    $user = User::find(12);
+
+    //create user
+//     $user = User::create([
+//     'name' => 'Tumi',
+//     'email' => 'Tumininu@gmail.com',
+//     'password' => 'Tumininu_123'
+// ]);
+
+// update
+/* $update = User::where('id',10)->update([
+    "name" => strtoupper("Funmi")
+    
+]); */
+
+    // Delete user
+    /* $delete = User::find(9);
+    $delete->delete(); */
+    dd($user->name);
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
